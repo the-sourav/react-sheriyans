ddd6b6e007e3985377b03bebcf9ea0b4ea45ead4
the-sourav <souravacharjya2001@gmail.com>
2026-05-14 19:50:44 +0530

    made eye catching card ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Card from "./components/Card";
+import picture1 from "./assets/images/picture1.webp";
+import picture2 from "./assets/images/picture2.jpg";
+import picture3 from "./assets/images/picture3.jpg";
 
 const App = () => {
   return (
-    <div className="parent">
-     <Card/>
-     <Card name="Ayan Shrama"/>
-     <Card name="Urvil Patel"/>
-     <Card name="Dewald Brevis"/>
-     <Card name="Payel Sharma"/>
-     <Card/>
+    <div className="container">
+      <Card  title="Modern Villa" description="A modern villa with stunning views. Best for a romantic getaway. Enjoy the luxurious amenities and breathtaking scenery." picture={picture1} rating="4.5" stay="3 Night Stay" />
 
+      <Card title="Swiss Chalet" description="Experience the beauty of Switzerland in this charming chalet. Cozy wooden chalet nestled in the Alps." picture={picture2} rating="4.8" stay="5 Night Stay" />
+
+      <Card title="Beachfront Bungalow" description="A beachfront bungalow with direct access to the ocean. Enjoy the sun, sand, and sea in this idyllic bungalow." picture={picture3} rating="4.7" stay="4 Night Stay" />
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
-const Card = ({ name = "Sourav Acharjya" }) => {
-  return (
-      <div className="card">
-        <h1>{name}</h1>
-        <p>Software Engineer</p>
-        <button>See Profile</button>
-      </div>
 
+const Card = ({title, description, picture, rating, stay}) => {
+  return (
+      <div className="villa-card">
+        <div className="villa-card__image">
+          <img src={picture} alt="Santorini Villa with ocean view" />
+        </div>
+
+        <div className="villa-card__content">
+          <h2 className="villa-card__title">{title}</h2>
+
+          <p className="villa-card__description">
+            {description}
+          </p>
+
+          <div className="villa-card__meta">
+            <span className="rating"> {rating} ★</span>
+            <span className="stay">{stay}</span>
+          </div>
+
+          <button className="reserve-btn">Reserve Now</button>
+        </div>
+      </div>
   );
 };
 
